d3519d54083e81056ca82055a2167126dc13621c
yuzhe <yu-zhe.kao@foxconn.com>
2024-06-03 15:05:19 +0800

    static display container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
+import Container from './components/Container';
 
 function App() {
   return (
     <div>
       <HelloWorld />
+      <Container />
     </div>
   );
 }
--- frontend/src/components/container.js
+++ frontend/src/components/container.js
@@ -4,89 +4,58 @@ import './components.css';
 
 function MessageBubble({role, timestamp, message}) {
 
-    return (
-        <div className="msg-bubble">
-            <div className="msg-info">
-                <div className="msg-info-name">{role}</div>
-                <div className="msg-info-time">{timestamp}</div>
-            </div>
-            <div className="msg-text">
-                {/* Hi, welcome to SimpleChat! Go ahead and send me a message. 😄 */}
-                {message}
-            </div>
-        </div>
-    );
+  return (
+    <div className="msg-bubble">
+      <div className="msg-info">
+        <div className="msg-info-name">{role}</div>
+        <div className="msg-info-time">{timestamp}</div>
+      </div>
+      <div className="msg-text">
+        {message}
+      </div>
+    </div>
+  );
 }
 
 function LeftMessage({role, timestamp, message}) {
-    return (
-        <div className="msg left-msg">
-            <div className="msg-img" style="background-image: url(https://image.flaticon.com/icons/svg/327/327779.svg)"> </div>
-
-            <MessageBubble role={"BOT"} timestamp={'12-15'}, message={'LeftMsg'}/>
-        </div>
-        
-    );
+  return (
+    <div className="msg left-msg">
+      <MessageBubble role={role} timestamp={timestamp} message={message}/>
+    </div>
+  );
 }
 
-function RightMessage() {
-    
+function RightMessage({role, timestamp, message}) {
+  return (
+    <div className="msg right-msg">
+      <MessageBubble role={role} timestamp={timestamp} message={message}/>
+    </div>
+  );
 }
 
 function Container() {
+  return (
+    <section className="msger">
+      <header className="msger-header">
+        <div className="msger-header-title">
+          <i className="fas fa-comment-alt"></i> SimpleChat
+        </div>
+        <div className="msger-header-options">
+          <span><i className="fas fa-cog"></i></span>
+        </div>
+      </header>
 
+      <main className="msger-chat">
+        <LeftMessage role={"BOT"} timestamp={"2024-06-03"} message={"How are you?"}/>
+        <RightMessage role={"USER"} timestamp={"2024-06-03 12:00:12"} message={"I am fine."}/>
+      </main>
+
+      <form className="msger-inputarea">
+        <input type={"text"} className={"msger-input"} placeholder={"Enter your message..."}/>
+        <button type="submit" className="msger-send-btn">Send</button>
+      </form>
+    </section>
+  );
 }
-<section class="msger">
-  <header class="msger-header">
-    <div class="msger-header-title">
-      <i class="fas fa-comment-alt"></i> SimpleChat
-    </div>
-    <div class="msger-header-options">
-      <span><i class="fas fa-cog"></i></span>
-    </div>
-  </header>
 
-  <main class="msger-chat">
-    <div class="msg left-msg">
-      <div
-       class="msg-img"
-       style="background-image: url(https://image.flaticon.com/icons/svg/327/327779.svg)"
-      ></div>
-
-      <div class="msg-bubble">
-        <div class="msg-info">
-          <div class="msg-info-name">BOT</div>
-          <div class="msg-info-time">12:45</div>
-        </div>
-
-        <div class="msg-text">
-          Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
-        </div>
-      </div>
-      
-    </div>
-
-    <div class="msg right-msg">
-      <div
-       class="msg-img"
-       style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)"
-      ></div>
-
-      <div class="msg-bubble">
-        <div class="msg-info">
-          <div class="msg-info-name">Sajad</div>
-          <div class="msg-info-time">12:46</div>
-        </div>
-
-        <div class="msg-text">
-          You can change your name in JS section!
-        </div>
-      </div>
-    </div>
-  </main>
-
-  <form class="msger-inputarea">
-    <input type="text" class="msger-input" placeholder="Enter your message...">
-    <button type="submit" class="msger-send-btn">Send</button>
-  </form>
-</section>
+export default Container;
